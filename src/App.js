920a8b807ffcb9dd53de48efaa3eb4fe33224055
Daim Khan <daimkhan01@outlook.com>
2024-06-25 20:22:57 +0500

    get the current date and time

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+const App = () => {
+  ///set current time///
+
+  let currTime = new Date().toLocaleTimeString();
+  let [crTime, setCtime] = useState(currTime);
+
+  let UpdateTime = () => {
+    currTime = new Date().toLocaleTimeString();
+    setCtime(currTime);
+  };
+  setInterval(UpdateTime, 1000);
+
+  ///set current date///
+  let currDate = new Date().toLocaleDateString();
+
+  ///set greeting with day effect///
+  let curDate = new Date();
+  curDate = curDate.getHours();
+  let greeting = "";
+  const cssStyle = {};
+
+  if (curDate >= 1 && curDate < 12) {
+    greeting = "Good Morning !";
+    cssStyle.color = "green";
+  } else if (curDate >= 12 && curDate < 19) {
+    greeting = "Good Afternoon !";
+    cssStyle.color = "orange";
+  } else {
+    greeting = "Good Night !";
+    cssStyle.color = "blue";
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="main_state">
+        <h1 className="count_num"> {crTime} </h1>
+        <h1 className="count_date">{currDate}</h1>
+        <h1 className="count_date">
+          <span className="greeting_style" style={cssStyle}>
+            {greeting}
+          </span>
+          Muhammad Daim
+        </h1>
+      </div>
+    </>
   );
-}
+};
 
 export default App;
